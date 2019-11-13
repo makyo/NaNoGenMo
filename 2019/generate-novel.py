2195acdb11cdcbaa6cdc8ebc78807cce437d74c3
Madison Scott-Clary <makyo@drab-makyo.com>
2019-11-12 22:15:31 -0800

    Woo!

--- 2019/generate-novel.py
+++ 2019/generate-novel.py
@@ -9,8 +9,8 @@ class Novel(object):
     """A generated novel in three parts."""
 
     def __init__(self, title, part_sources, chapter_intro_source,
-                 max_paragraph_length=10, max_chapter_length=100,
-                 chapters_per_part=10, intro_lines=8):
+                 max_paragraph_length=10, max_chapter_length=40,
+                 chapters_per_part=8, intro_lines=5):
         self.title = title
         self.part_sources = map(self._create_model, part_sources)
         self.chapter_intro_source = self._create_model(chapter_intro_source)
@@ -27,10 +27,10 @@ class Novel(object):
                 for i in range(0, self.chapters_per_part)])
 
     def _generate_chapter(self, source):
-        intro = [self._make_sentence(self.chapter_intro_source)
+        intro = [self._make_sentence(self.chapter_intro_source,start='You')
                  for i in range(0, self.intro_lines)]
         num_paragraphs = random.randint(
-            self.max_chapter_length / 4 * 3, self.max_chapter_length)
+            self.max_chapter_length * (4//3), self.max_chapter_length)
         body = [
             ' '.join([
                 self._make_sentence(source)
@@ -38,10 +38,13 @@ class Novel(object):
             ]) for i in range(0, num_paragraphs)]
         return {'intro': intro, 'body': body}
 
-    def _make_sentence(self, source):
+    def _make_sentence(self, source, start=None):
         sentence = None
         while sentence is None:
-            sentence = source.make_sentence()
+            if start:
+                sentence = source.make_sentence_with_start(start)
+            else:
+                sentence = source.make_sentence()
         return sentence
 
     def _create_model(self, corpus):
@@ -66,6 +69,27 @@ class Novel(object):
             part_index += 1
         return md
 
+    def to_latex(self):
+        if len(self.parts) == 0:
+            raise self.NotYetGenerated
+        # md = '\\title{%s}\n\n' % self.title
+        md = ''
+        part_index = 1
+        for part in self.parts:
+            chapter_index = 1
+            md += '\\part{}\n\n'
+            for chapter in part:
+                md += '\\chapter{}\n\n'
+                md += '\\begin{verse}\n'
+                for line in chapter['intro']:
+                    md += '{} \\\\\n'.format(line)
+                md += '\\end{verse}\n\n'
+                for paragraph in chapter['body']:
+                    md += '{}\n\n'.format(paragraph)
+                chapter_index += 1
+            part_index += 1
+        return md
+
     class NotYetGenerated(Exception):
         pass
 
@@ -84,4 +108,4 @@ if __name__ == '__main__':
     corpora = get_corpora(sys.argv[3:])
     novel = Novel(sys.argv[1], corpora, intro_corpus)
     novel.generate()
-    print(novel.to_markdown())
+    print(novel.to_latex())
